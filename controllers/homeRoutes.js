2534231b1b1c051fbc9bc24bf6930dfdcbb641aa
Naji Dia <najimdia@gmail.com>
2022-01-09 22:52:15 -0800

    fixed dynamic api call

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
+const axios = require("axios");
 const { User, Coins } = require("../models");
 const withAuth = require("../utils/auth");
+const apiKey = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,USDT,BNB,SOL,USDC,ADA,XRP,LUNA,DOT,AVAX,DOGE,SHIB,MATIC,BUSD,CRO,WBTC,LINK,UST,UNI&tsyms=USD,EUR&api_key=06cdcb1f8cd5ced9c1a2b7a5acf8be80d74315bd49d57263cfee49051f2460b3"
 
 router.get('/', async (req, res) => {
   try {
@@ -15,10 +17,12 @@ router.get('/homepage', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const coinsData = await Coins.findAll();
-    
-    const coins = coinsData.map((coins) => coins.get({ plain: true }))
 
-    console.log(coins)
+    const coins = coinsData.map((coins) => coins.get({ plain: true }))
+    const apiData = await axios.get(apiKey)
+    for (let i = 0; i < coins.length; i++) {
+      coins[i].currentPrice = apiData.data[coins[i].ticker].USD
+    }
 
     res.render('homepage', {
       coins,
@@ -61,3 +65,18 @@ router.get("/login", (req, res) => {
 });
 
 module.exports = router;
+
+
+const coin = {
+  id: 4,
+  img: '/imgs/BNB-logo.png',
+  name: 'Binance Coin',
+  ticker: 'BNB',
+  walletId: null,
+  wallet_id: null,
+  newThing: 'whatever',
+  dynamicPrice: 414123
+}
+
+coin.newThing = 'whatever'
+
