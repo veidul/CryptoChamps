26df01d718561ae36df516d909fe7831d1205326
ludie lambright <ludiefletcher@aol.com>
2022-01-12 16:20:21 -0800

    fixed issue with placement of if statement returning from function too late. if you didnt have enough money it would still push data to array then get added to list. ** also moved functions over to helpers.js from coins.handlebars

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -1,8 +1,14 @@
-const getPrice = function(ticker){
-    const data = ticker;
-    const price = `{{coinPrice.${data}.USD}}`;
-   return price
-}
+const tickers = [];
+let walletTotalCost = 0
+  var bank = 75000
+
+
+  const updateCost = (event) =>{ 
+  const val = event.target.value;
+  const price = event.target.dataset.price
+  const total = val*price;
+  const costOutput = event.target.parentElement.lastElementChild.textContent = `$ ${total}`
+  }
 
 const walletCost = (tickers) => {
     let total = 0
@@ -37,7 +43,25 @@ const tickerChecker = ({ticker,price,quantity,total}) => {
 const walletDisplay = (tickers) => {
     let string = ""
     for(let i = 0; i < tickers.length; i++){
-        string += `<li><h1>Ticker: ${tickers[i].ticker}</p><p>Purchase Price: ${tickers[i].price}</p><p>Total Cost: ${tickers[i].total}</p><p>Quantity: ${tickers[i].quantity}<p><li>`
+        string += `<li><h1>Ticker: ${tickers[i].ticker}</p><p>Purchase Price:$ ${tickers[i].price}</p><p>Total Cost:$ ${tickers[i].total}</p><p>Quantity: ${tickers[i].quantity}<p><li>`
     }
     return string
 }
+
+const addToWallet = (event) => {
+    console.log(bank,"bank log")
+    let val = event.target.parentElement.children[2].children[3].value;
+    let wallet = document.querySelector('.wallet-list')
+    let price = event.target.dataset.price
+    let ticker = event.target.dataset.ticker
+    const total = val*price;
+    if(bank < total) return alert("NOT ENOUGH!");
+    tickerChecker({ticker,price,quantity:val,total})
+    var div = document.createElement('div');
+    div.innerHTML = walletDisplay(tickers);
+    document.querySelector('.walletTotal').textContent = (`$ ${walletCost(tickers)}`)
+    document.querySelector('.bankTotal').textContent = (`$ ${bankCost(total,bank)}`)
+    console.log(bank,"bank log#2")
+    wallet.innerHTML = "";
+    wallet.append(div)
+  }
