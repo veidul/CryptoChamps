c4e3540ded8b2fdf2664d42b668075b13825c8fc
scorpion77777 <vvmartirosyan@gmail.com>
2022-01-13 16:30:29 -0800

    front end

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
+
 const userRoutes = require("./userRoutes");
-const tourneyRoutes = require("./tourneyRoutes")
+const tourneyRoutes = require("./tourneyRoutes");
 const walletRoutes = require("./walletRoutes");
 
 router.use("/users", userRoutes);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: ['views/layouts/main.handlebars'],
+  content: ["views/layouts/main.handlebars"],
+  plugins: [require("@tailwindcss/forms")],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms'),],
-}
+};
