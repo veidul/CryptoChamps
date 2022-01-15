178058742317b37d422076250f2c1b6cd72626f7
Naji Dia <najimdia@gmail.com>
2022-01-15 10:54:04 -0800

    ok

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
+const seeder = require("../seeds");
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.get('/seeder', async(req,res)=> {
+    if(req.query.secret === process.env.SECRET){
+        await seeder();
+        res.send("seeded!")
+    }
+})
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -16,5 +16,5 @@ const seedAll = async () => {
   process.exit(0);
 };
 
-seedAll();
+module.exports = seedAll;
 
