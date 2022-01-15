d8ee40db152790662675de1876c97ff88993a79e
Naji Dia <najimdia@gmail.com>
2022-01-15 10:09:59 -0800

    styling changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const tourneyRoutes = require("./tourneyRoutes")
-const walletRoutes = require("./walletRoutes");
+
 
 router.use('/users', userRoutes);
 router.use('/tournament', tourneyRoutes);
-router.use('/wallet', walletRoutes);
+
 
 module.exports = router;
--- seeds/tournament-seed.js
+++ seeds/tournament-seed.js
@@ -2,24 +2,24 @@ const { Tourney } = require('../models');
 
 const tourneyData = [
     {
-        startTime: "2022-01-16 10:30:00",
-        finishTime: "2022-01-17 10:30:00"
+        startTime: "2022-02-13 10:30:00",
+        finishTime: "2022-02-20 10:30:00"
+    },
+    {
+        startTime: "2022-02-22 10:30:00",
+        finishTime: "2022-01-28 10:30:00"
     },
     {
         startTime: "2022-01-16 10:30:00",
-        finishTime: "2022-01-17 10:30:00"
+        finishTime: "2022-01-22 10:30:00"
     },
     {
-        startTime: "2022-01-16 10:30:00",
-        finishTime: "2022-01-17 10:30:00"
+        startTime: "2022-01-23 10:30:00",
+        finishTime: "2022-01-29 10:30:00"
     },
     {
-        startTime: "2022-01-16 10:30:00",
-        finishTime: "2022-01-17 10:30:00"
-    },
-    {
-        startTime: "2022-01-16 10:30:00",
-        finishTime: "2022-01-17 10:30:00"
+        startTime: "2022-01-20 10:30:00",
+        finishTime: "2022-02-5 10:30:00"
     },
     {
         startTime: new Date(Date.now() + 3600000*24),
